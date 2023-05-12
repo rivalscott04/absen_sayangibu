81508f1471fb1b1710f80aa595235d27aa3df621
ikhzay <ikhzay@gmail.com>
2023-05-12 19:39:14 +0800

    CRUD Siswa

---
+++ database/migrations/2023_05_11_030109_create_absen_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absen', function (Blueprint $table) {
+            $table->id();
+            // $table->timestamps();
+            $table->string('kartu_id');
+            $table->date('tanggal');
+            $table->string('kelas');
+            $table->time('jam_masuk');
+            $table->time('jam_absen');
+            $table->integer('no_mesin');
+            $table->integer('status');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absen');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::group(['prefix' => 'api'], function () {
+
+//     // SISWA
+//     Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+//     Route::get('/siswa/{id}', [SiswaController::class, 'get'])->name('siswa.get');
+//     Route::get('/siswa/nis/{nis}', [SiswaController::class, 'getByNis'])->name('siswa.getByNis');
+//     Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+//     Route::put('/siswa/{id}', [SiswaController::class, 'update'])->name('siswa.update');
+//     Route::delete('/siswa/{id}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+// });
